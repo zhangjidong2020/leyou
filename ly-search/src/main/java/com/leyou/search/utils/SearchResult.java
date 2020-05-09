37951835172bbc1d1e28643ae8d057501b19ab9b
danny8000 <527686617@qq.com>
2020-05-09 16:47:34 +0800

    第一次提交

--- ly-search/src/main/java/com/leyou/search/utils/SearchResult.java
+++ ly-search/src/main/java/com/leyou/search/utils/SearchResult.java
@@ -21,4 +21,28 @@ public class SearchResult extends PageResult<Goods> {
         this.brands = brands;
         this.specs = specs;
     }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public List<Brand> getBrands() {
+        return brands;
+    }
+
+    public void setBrands(List<Brand> brands) {
+        this.brands = brands;
+    }
+
+    public List<Map<String, Object>> getSpecs() {
+        return specs;
+    }
+
+    public void setSpecs(List<Map<String, Object>> specs) {
+        this.specs = specs;
+    }
 }
